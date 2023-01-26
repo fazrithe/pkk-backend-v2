76a62d9257f35b2d806cc23fcd9d4e92847738b2
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-01-26 10:54:22 +0700

    get data institutions

--- controllers/institution.controller.go
+++ controllers/institution.controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -47,4 +48,36 @@ func (ic *InstitutionController) CreateInstitution(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "data": newInstitution})
+}
+
+func (ic *InstitutionController) FindInstitutions(ctx *gin.Context) {
+	var page = ctx.DefaultQuery("page", "1")
+	var limit = ctx.DefaultQuery("limit", "10")
+
+	intPage, _ := strconv.Atoi(page)
+	intLimit, _ := strconv.Atoi(limit)
+	offset := (intPage - 1) * intLimit
+
+	var institutions []models.Institution
+	result := ic.DB.Limit(intLimit).Offset(offset).Find(&institutions)
+	if result.Error != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": result.Error})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "results": len(institutions), "data": institutions})
+}
+
+func (ic *InstitutionController) FindInstitutionById(ctx *gin.Context) {
+	institutionId := ctx.Param("institutionId")
+
+	var institution models.Institution
+	result := ic.DB.First(&institution, "id = ?", institutionId)
+	if result.Error != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": "No Insitution with that name exists"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": institution})
+
 }
--- routes/institution.routes.go
+++ routes/institution.routes.go
@@ -18,4 +18,6 @@ func (ic *InstitutionRouteController) InstitutionRoute(rg *gin.RouterGroup) {
 	router := rg.Group("institutions")
 	router.Use(middleware.DeserializeUser())
 	router.POST("/", ic.institutionController.CreateInstitution)
+	router.GET("/", ic.institutionController.FindInstitutions)
+	router.GET("/:institutionId", ic.institutionController.FindInstitutionById)
 }
